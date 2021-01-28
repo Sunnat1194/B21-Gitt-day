bb6ae1789bee0ae4f2d1eaf83ca7f790609c3529
day1 <Sunnat1194@gmail.com>
2021-01-28 00:14:25 -0500

    B21 learning git

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,6 +3,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-
+        System.out.println("B21 future SDET learning git");
     }
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,6 @@ public class HelloTest {
 
     //I'm not sure what i'm doing lol
     //I need a lot of practice
-    //I'm so lazy 
+    //I'm so lazy
 
 }
